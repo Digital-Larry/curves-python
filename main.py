99eea70efbb1bd4ea3357e759c03a95ed2fad2b1
Digital-Larry <faultline1989@yahoo.com>
2013-02-03 11:51:29 -0800

    Added spirograph and harmonograph drawing routines.
    See notes for curve.py.

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # Requires Python Curve class (curve.py)
 # Written by Gary S. Worsham
 # January 3, 2013
+# Revision February 3, 2013
+# - Added Spirograph and Harmonograph classes.
+# - previous main curve is now "Lissajous" class.
+# -------------------------------------------------
 # This code covered by the GPLV3 license.
 # See GPLV3.txt for details.
 
-import pygame, sys, os, math, random
+import pygame, sys, os, math, random, time
 from pygame.locals import *
 import curve
 
@@ -18,19 +22,17 @@ except ImportError:
 
 class App():
     def __init__(self):
-        self.width = 800
-        self.height = 600 
-        self.xmax = 0;
-        self.ymax = 0;
+        self.width = 640
+        self.height = 480
         
         pygame.init()
         pygame.display.set_caption("Python Curve Generator")   
         error = pygame.display.set_mode((self.width, self.height))
         print error
         self.screen = pygame.display.get_surface()
-
-        self.Curve1 = curve.Curve(self.width, self.height, self.screen)
-        self.Curve2 = curve.Curve(self.width, self.height, self.screen)
+        # define 2 curves and select their functions intially
+        self.curveSet = curve.CurveSet(self.width, self.height, self.screen)
+        self.curveSet.selectCurves() 
 
         # Map the back button to the escape key.
         if android:
@@ -40,12 +42,12 @@ class App():
         if android: 
             self.delay = 5
         else:
-            self.delay = 50
+            self.delay = 2
 
     def clearScreen(self):
-      # self.screen.fill((random.randint(1,15), random.randint(2, 10), random.randint(1,50)))
-      self.screen.fill((0, 0, 0))
-      pygame.display.flip()   
+        # self.screen.fill((random.randint(1,15), random.randint(2, 10), random.randint(1,50)))
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()   
 
     def input(self, events): 
       for event in events: 
@@ -55,14 +57,14 @@ class App():
              if event.type == pygame.MOUSEBUTTONDOWN:
                print ("Android key down!")
                self.clearScreen()
-               self.randomize()
+               self.selectCurve()
                  
          if event.type == KEYDOWN:
             if event.key == K_1:
                print ("Key 1")
             if event.key == K_t:
                print ("Key t")
-               self.testAll()
+               curve.testAll()
             if event.key == K_x:
                print ("Key x")
                self.testX()
@@ -72,39 +74,104 @@ class App():
             if event.key == K_s:
                print ("Key s")
                self.saveScreen()
+            if event.key == K_q:
+               print ("Key q")
+               self.sequence()
             if event.key == K_SPACE:
                print ("Key Space!")
                self.time = 0.0
+               self.seconds = 0.0
                self.clearScreen()
-               self.Curve1.randomize()
-               self.Curve2.randomize()
+               # self.saveScreen()
+               self.curveSet.selectCurves()
             if event.key == K_ESCAPE:
                print ("Escape")
                sys.exit(0)
          else:
             x = 1
             # print event
-
-    def saveScreen(self):
-        print "Saving screen!"
+            
+    def saveScreen(self, filename, foldername="curvedata-01"):
+        print "Saving screen!", foldername, filename
         font = pygame.font.Font(None, 20)
         name_fg = (220, 250, 235)
-        # factors = 'Factors-{}{}{}{}-{}{}={}{}{}{}-{}{}.jpg'.format(self.fct1_1, self.fct1_2, self.fct1_3, self.fct1_4, self.fct1_5, self.fct1_6,self.fct2_1, self.fct2_2, self.fct2_3, self.fct2_4, self.fct2_5, self.fct2_6)
-        filename = 'Circle1-x-{}-y-{}.jpg'.format(self.Curve1.xFunc, self.Curve1.yFunc)
-        print filename
-        text = font.render(filename, 1, name_fg, (0, 0, 0))
-        textpos = text.get_rect()
-        # print self.name, textpos, textpos.centerx
-        textpos.centerx = self.width/2
-        textpos.centery = self.height - 20
-        # print self.name, textpos, textpos.centerx, textpos.centery
-        self.screen.blit(text, textpos)
-        pygame.image.save(self.screen, filename)
+        fileName = '{}\\{:03d}.jpg'.format(foldername, filename)
+        pygame.image.save(self.screen, fileName)
 
+    def run(self):
+        self.seconds = 0.0
+        self.clearScreen()
+        self.delay = 10
+        folderName = "random"
+        fileIndex = 0
+
+        # print "Hello:"
+        while True: 
+            self.input(pygame.event.get())
+            i = self.curveSet.drawPoints()
+            if (self.seconds >= 20000.0) | (i == -1):
+                print "Seconds: ", self.seconds
+                self.saveScreen(fileIndex, folderName)
+                self.clearScreen()
+                self.curveSet.selectCurves()
+                self.seconds = 0.0
+                fileIndex += 1
+
+            pygame.time.wait(self.delay)
+            self.seconds = self.seconds + 1.0
+            pygame.display.flip()
+         
+        pygame.quit()   
+        pygame.exit()
+
+    def sequence(self):
+        seconds = 0
+        self.clearScreen()
+        self.delay = 1
+        curveSequence = curve.Sequence(self.width, self.height, self.screen)
+        folderName = "curvedata"
+        fileIndex = 0
+        
+        # print "Hello:"
+        for i in range (0, 400):
+            print "Sequence step ", i
+            while seconds < 2000:
+                key = self.input(pygame.event.get())
+                if (key == K_SPACE):
+                    self.fileIndex = 0
+                    self.clearscreen()
+                    curveSequence.randomize()
+                    curveSequence.time = 0.0
+                    seconds = 0                 
+
+                curveSequence.drawPoint()
+                pygame.time.wait(1)
+                seconds = seconds + 1
+                pygame.display.flip()
+
+            # print "Seconds: ", self.seconds
+            self.saveScreen(fileIndex, folderName)
+            fileIndex += 1
+            self.clearScreen()
+            curveSequence.adjust()
+            seconds = 0
+
+         
+#-----------------------------------------------------------------     
+def main(param):
+   app = App()
+   i = param
+   app.run()
+
+# This isn't run on Android.
+if __name__ == "__main__":
+    main(1)
+
+"""
     def testAll(self):
        self.radFunc1 = 0
        self.colorFunc1 = 0
-       self.randomize()
+       self.selectCurve()
 
        self.clearScreen()
        
@@ -123,12 +190,12 @@ class App():
                     pygame.display.flip()
                 self.saveScreen()
                 self.clearScreen()
-                self.randomize()
+                self.selectCurve()
 
     def testX(self):
         self.radFunc1 = 1
         self.colorFunc1 = 1
-        self.randomize()
+        self.selectCurve()
 
         self.clearScreen()
         self.yFunc1 = 8
@@ -152,12 +219,12 @@ class App():
                 pygame.display.flip()
             self.saveScreen()
             self.clearScreen()
-            self.randomize()
+            self.selectCurve()
 
     def testY(self):
         self.radFunc1 = 0
         self.colorFunc1 = 0
-        self.randomize()
+        self.selectCurve()
 
         self.clearScreen()
 
@@ -165,7 +232,7 @@ class App():
             self.xFunc1 = 8
             self.yFunc1 = j
             time = 0.0
-            speed = 500
+            speed = 1000
             increment = 6.283/speed 
 
             while (time < 2000.0):
@@ -175,44 +242,6 @@ class App():
                 pygame.display.flip()
             self.saveScreen()
             self.clearScreen()
-            self.randomize()
+            self.selectCurve()
                              
-    def run(self):
-       self.time = 0.0
-       self.speed = 500
-       increment = 6.283/self.speed 
-
-       self.clearScreen()
-       self.Curve1.randomize()
-       self.Curve2.randomize()
-       seconds = 0.0
-      
-      # print "Lehho:"
-       while True: 
-         self.input(pygame.event.get()) 
-         self.Curve1.drawFunc(self.time)
-         self.Curve2.drawFunc(self.time)
-         if (seconds >= 20000.0):
-            print "Seconds: ", seconds
-            self.clearScreen()
-            self.Curve1.randomize()
-            self.Curve2.randomize()
-            delay = random.randint(35, 50)
-            seconds = 0.0
-         self.time = self.time + increment
-         pygame.time.wait(self.delay)
-         seconds = seconds + 1000.0/self.delay
-         pygame.display.flip()
-         
-       pygame.quit()   
-       pygame.exit()
-
-#-----------------------------------------------------------------     
-def main(param):
-   app = App()
-   i = param
-   app.run()
-
-# This isn't run on Android.
-if __name__ == "__main__":
-    main(1)
+"""
